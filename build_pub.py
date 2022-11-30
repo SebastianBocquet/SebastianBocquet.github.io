ea1245e3812dc52d4c7de0b457dd0aa831872568
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2022-11-30 11:05:37 +0100

    DES pubs

--- build_pub.py
+++ build_pub.py
@@ -11,7 +11,8 @@ DES_builder_bibcodes = [ '2022arXiv220111142M', '2022arXiv220311306K', '2022arXi
                          '2022arXiv220705766D', '2022arXiv220711937S', '2022arXiv220805491Y',
                          '2022MNRAS.515.4587W', '2022arXiv220905519G', '2022arXiv220905853G',
                          '2022arXiv220906897C', '2022arXiv220909782E', '2022arXiv221003977M',
-                         '2022arXiv221005057C', '2022arXiv221008633S', '2022arXiv221010802Z']
+                         '2022arXiv221005057C', '2022arXiv221008633S', '2022arXiv221010802Z',
+                         '2022arXiv221114291D']
 white_paper_bibcodes = ['2019BAAS...51c.279M', '2022arXiv220306795B', '2022arXiv220308024A']
 
 ads_prefix = "https://ui.adsabs.harvard.edu/abs/"
