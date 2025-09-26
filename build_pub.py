d2279a4d17fa21f7c2dfe0a1986162a871701b08
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2025-09-26 20:36:07 +0200

    explicit list of DES builder pubs

--- build_pub.py
+++ build_pub.py
@@ -8,13 +8,25 @@ DES_coauthor_bibcodes = ['2021MNRAS.507.5758S', 'arXiv:2105.13541', 'arXiv:2202.
                          'arXiv:2310.00059', 'arXiv:2310.13207', 'arXiv:2311.07512', 'arXiv:2304.10128',
                          'arXiv:2404.02153', 'arXiv:2407.10961', 'arXiv:2503.13631', 'arXiv:2503.13632',
                          'arXiv:2503.17271', 'arXiv:2502.12914', 'arXiv:2509.02068']
-DES_builder_bibcodes = ['arXiv:2309.00671', 'arXiv:2309.06593', '2023MNRAS.521..836S', '2023AJ....165..222L',
-                        '2023MNRAS.522.1951D', '2023MNRAS.522.3163S', 'arXiv:2304.13570', '2023MNRAS.522.4132Y',
-                        'arXiv:2305.17173', 'arXiv:2401.02929', 'arXiv:2401.12049', 'arXiv:2402.10697',
-                        'arXiv:2402.10696', 'arXiv:2403.02314', 'arXiv:2404.06098', 'arXiv:2404.18278',
-                        'arXiv:2405.10881', '2024MNRAS.534.2328D', '2024ApJ...973L..14D',
-                        '2025MNRAS.537.1818C', 'arXiv:2501.06664',
-                        ]
+DES_builder_bibcodes = ['arXiv:2112.01541', 'arXiv:2201.05227', 'arXiv:2201.11740', 'arXiv:2201.02762',
+                        '2023MNRAS.521..836S',
+                        'arXiv:2203.16565', 'arXiv:2201.11142', 'arXiv:2203.07128', 'arXiv:2203.11306',
+                        'arXiv:2207.05583', 'arXiv:2202.10480', 'arXiv:2206.12085', 'arXiv:2210.05057',
+                        'arXiv:2204.05924', 'arXiv:2206.06928', 'arXiv:2209.06897', 'arXiv:2203.12439',
+                        'arXiv:2203.12440', 'arXiv:2206.10824', 'arXiv:2206.08591', 'arXiv:2208.01357',
+                        'arXiv:2207.11937', 'arXiv:2207.08520', 'arXiv:2210.03977', 'arXiv:2207.05766',
+                        'arXiv:2209.05519', 'arXiv:2202.08233', 'arXiv:2304.01858', 'arXiv:2212.03734',
+                        'arXiv:2303.15156', 'arXiv:2210.08633', 'arXiv:2208.05491', 'arXiv:2304.13570',
+                        'arXiv:2209.09782', 'arXiv:2210.10802', 'arXiv:2212.11319', 'arXiv:2309.06593',
+                        'arXiv:2305.17173', 'arXiv:2211.16593', 'arXiv:2211.14291', 'arXiv:2302.04878',
+                        'arXiv:2209.05853', 'arXiv:2309.00671', 'arXiv:2406.05047', 'arXiv:2408.00922',
+                        'arXiv:2401.02929', 'arXiv:2406.05048', 'arXiv:2406.05050', 'arXiv:2402.10697',
+                        'arXiv:2402.10696', 'arXiv:2404.06098', 'arXiv:2410.22272', 'arXiv:2404.18278',
+                        'arXiv:2401.12049', 'arXiv:2410.07956', 'arXiv:2403.02314', 'arXiv:2406.12675',
+                        'arXiv:2406.05049', 'arXiv:2405.10881', 'arXiv:2503.03964', 'arXiv:2503.06712',
+                        'arXiv:2409.04535', 'arXiv:2408.01001', 'arXiv:2410.20205', 'arXiv:2506.07432',
+                        'arXiv:2506.13439', 'arXiv:2507.21459', 'arXiv:2501.06664', 'arXiv:2508.14018',
+                        'arXiv:2509.07943', 'arXiv:2509.07964', 'arXiv:2509.18967',]
 white_paper_bibcodes = ['2019BAAS...51c.279M', '2022arXiv220306795B', '2022arXiv220308024A']
 skip_bibcodes = []
 
@@ -51,9 +63,9 @@ def main(ads_token):
             refereed+= 1
 
     # List of DES papers
-    ref = "https://dbweb2.fnal.gov:8443/DESPub/app/PB/pub/pbpublished"
-    f = requests.get(ref, timeout=6.1)
-    DES_paper_list = f.text
+    # ref = "https://dbweb2.fnal.gov:8443/DESPub/app/PB/pub/pbpublished"
+    # f = requests.get(ref, timeout=6.1)
+    # DES_paper_list = f.text
 
     # Publication list
     top_tier_list, co_pub_list, DES_pub_list, white_paper_list, other_pub_list = [], [], [], [], []
@@ -102,10 +114,6 @@ def main(ads_token):
                     for bibcode in paper['identifier']:
                         if bibcode in DES_builder_bibcodes:
                             pub_type = 'DES'
-                        elif bibcode[4:9]=='arXiv':
-                            code = '.'.join((bibcode[9:13],bibcode[13:18]))
-                            if (float(code[:2])>=21) and (code in DES_paper_list):
-                                pub_type = 'DES'
                 # White papers
                 for bibcode in paper['identifier']:
                     if bibcode in white_paper_bibcodes:
